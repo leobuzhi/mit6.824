8d75699103822778753cdd7737d04e5e2dd14ef1
leobuzhi <leobuzhi@gmail.com>
2018-12-04 08:33:45 +0800

    raft: add test TestFigure82C

--- raft/raft_test.go
+++ raft/raft_test.go
@@ -633,3 +633,69 @@ func TestPersist32C(t *testing.T) {
 
 	cfg.end()
 }
+
+//
+// Test the scenarios described in Figure 8 of the extended Raft paper. Each
+// iteration asks a leader, if there is one, to insert a command in the Raft
+// log.  If there is a leader, that leader will fail quickly with a high
+// probability (perhaps without committing the command), or crash after a while
+// with low probability (most likey committing the command).  If the number of
+// alive servers isn't enough to form a majority, perhaps start a new server.
+// The leader in a new term may try to finish replicating log entries that
+// haven't been committed yet.
+//
+func TestFigure82C(t *testing.T) {
+	servers := 5
+	cfg := makeConfig(t, servers, false)
+	defer cfg.cleanup()
+
+	cfg.begin("Test (2C): Figure 8")
+
+	cfg.one(rand.Int(), 1, true)
+
+	nup := servers
+	for iters := 0; iters < 1000; iters++ {
+		leader := -1
+		for i := 0; i < servers; i++ {
+			if cfg.rafts[i] != nil {
+				_, _, ok := cfg.rafts[i].Start(rand.Int())
+				if ok {
+					leader = i
+				}
+			}
+		}
+
+		if (rand.Int() % 1000) < 100 {
+			ms := rand.Int63() % (int64(RaftElectionTimeout/time.Millisecond) / 2)
+			time.Sleep(time.Duration(ms) * time.Millisecond)
+		} else {
+			ms := (rand.Int63() % 13)
+			time.Sleep(time.Duration(ms) * time.Millisecond)
+		}
+
+		if leader != -1 {
+			cfg.crash1(leader)
+			nup--
+		}
+
+		if nup < 3 {
+			s := rand.Int() % servers
+			if cfg.rafts[s] == nil {
+				cfg.start1(s)
+				cfg.connect(s)
+				nup++
+			}
+		}
+	}
+
+	for i := 0; i < servers; i++ {
+		if cfg.rafts[i] == nil {
+			cfg.start1(i)
+			cfg.connect(i)
+		}
+	}
+
+	cfg.one(rand.Int(), servers, true)
+
+	cfg.end()
+}
